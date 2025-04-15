a550415043cfe92769c2222a6e94044448739358
Niharika086 <niharika08.2004@gmail.com>
2025-04-15 16:42:08 +0530

    Added call directory files

---
+++ .history/src/test/java/com/scm/ApplicationTests_20250415163056.java
@@ -0,0 +1,20 @@
+package com.scm;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.scm.services.EmailService;
+
+@SpringBootTest
+class ApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/scm/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class ApplicationTests {
+
+
+	void contextLoads() {
+	}
+
+}
